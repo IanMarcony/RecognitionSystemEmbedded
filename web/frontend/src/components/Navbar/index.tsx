3ed8d5b59d5531fb04aeb779819c2c0fd0438f79
Kirk <ksahdo10@gmail.com>
2024-07-04 17:08:52 -0400

    update: add navbar links

--- web/frontend/src/components/Navbar/index.tsx
+++ web/frontend/src/components/Navbar/index.tsx
@@ -1,12 +1,17 @@
-import { Navbar as Nav, Container } from "react-bootstrap";
+import { Navbar as NavBar, Nav, Container } from "react-bootstrap";
 
 const Navbar = () => {
   return (
-    <Nav bg="dark" data-bs-theme="dark" className="bg-body-tertiary">
+    <NavBar bg="primary" data-bs-theme="dark">
       <Container>
-        <Nav.Brand href="#home"> Dashboard - Belt System</Nav.Brand>
+        <NavBar.Brand href="/">Belt System</NavBar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/">Dashboard</Nav.Link>
+          <Nav.Link href="/categories">Categories</Nav.Link>
+          <Nav.Link href="/objects">Objects</Nav.Link>
+        </Nav>
       </Container>
-    </Nav>
+    </NavBar>
   );
 };
 
