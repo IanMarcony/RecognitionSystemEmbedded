14297bc5558c6bab00d1eca80a422de7dc13356d
Ian Salazar <ian.salazar@itriad.org.br>
2024-07-08 09:54:40 -0400

    fix: change order cross_origin annotation

--- web/backend/src/app/modules/logs/routes/index.py
+++ web/backend/src/app/modules/logs/routes/index.py
@@ -4,7 +4,7 @@ from ..controllers.index import LogsController
 logs_routes = Blueprint('logs', __name__)
 logs_controller = LogsController()
 
-@cross_origin(supports_credentials=True)
 @logs_routes.route('/', methods=['GET'])
+@cross_origin(supports_credentials=True)
 def get_logs():
     return logs_controller.index()
--- web/backend/src/app/modules/products/routes/index.py
+++ web/backend/src/app/modules/products/routes/index.py
@@ -4,28 +4,28 @@ from ..controllers.index import ProductsController
 products_routes = Blueprint('products', __name__)
 products_controller = ProductsController()
 
-@cross_origin(supports_credentials=True)
 @products_routes.route('/', methods=['GET'])
+@cross_origin(supports_credentials=True)
 def get_products():
     print('get_products')
     return products_controller.index()
 
-@cross_origin(supports_credentials=True)
 @products_routes.route('/<int:product_id>', methods=['GET'])
+@cross_origin(supports_credentials=True)
 def get_unique_product(product_id):
     return products_controller.get_unique(product_id)
 
-@cross_origin(supports_credentials=True)
 @products_routes.route('/', methods=['POST'])
+@cross_origin(supports_credentials=True)
 def create_product():
     return products_controller.create(request)
 
-@cross_origin(supports_credentials=True)
 @products_routes.route('/<int:product_id>', methods=['PUT'])
+@cross_origin(supports_credentials=True)
 def change_product(product_id):
     return products_controller.change(request, product_id)
 
-@cross_origin(supports_credentials=True)
 @products_routes.route('/<int:product_id>', methods=['DELETE'])
+@cross_origin(supports_credentials=True)
 def delete_product(product_id):
     return products_controller.delete(product_id)
