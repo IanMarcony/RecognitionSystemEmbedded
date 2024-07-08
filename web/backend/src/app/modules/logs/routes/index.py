9ef62ed4258555f5a83522dd74d493ebf7dc45ee
Ian Salazar <ian.salazar@itriad.org.br>
2024-07-07 23:40:11 -0400

    fix: add cross_origin fixing acess from react front

--- web/backend/src/app/modules/logs/routes/index.py
+++ web/backend/src/app/modules/logs/routes/index.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, request
+from flask_cors import cross_origin
 from ..controllers.index import LogsController
 logs_routes = Blueprint('logs', __name__)
 logs_controller = LogsController()
 
+@cross_origin(supports_credentials=True)
 @logs_routes.route('/', methods=['GET'])
 def get_logs():
     return logs_controller.index()
--- web/backend/src/app/modules/products/routes/index.py
+++ web/backend/src/app/modules/products/routes/index.py
@@ -1,25 +1,31 @@
 from flask import Blueprint, request
+from flask_cors import cross_origin
 from ..controllers.index import ProductsController
 products_routes = Blueprint('products', __name__)
 products_controller = ProductsController()
 
+@cross_origin(supports_credentials=True)
 @products_routes.route('/', methods=['GET'])
 def get_products():
     print('get_products')
     return products_controller.index()
 
+@cross_origin(supports_credentials=True)
 @products_routes.route('/<int:product_id>', methods=['GET'])
 def get_unique_product(product_id):
     return products_controller.get_unique(product_id)
 
+@cross_origin(supports_credentials=True)
 @products_routes.route('/', methods=['POST'])
 def create_product():
     return products_controller.create(request)
 
+@cross_origin(supports_credentials=True)
 @products_routes.route('/<int:product_id>', methods=['PUT'])
 def change_product(product_id):
     return products_controller.change(request, product_id)
 
+@cross_origin(supports_credentials=True)
 @products_routes.route('/<int:product_id>', methods=['DELETE'])
 def delete_product(product_id):
     return products_controller.delete(product_id)
