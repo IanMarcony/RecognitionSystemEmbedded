bcef0d2c2d9926c080843ca40f093515c3f59fdc
Kirk <ksahdo10@gmail.com>
2024-07-15 00:40:03 -0400

    feat: add dashboard cam component

--- web/frontend/src/pages/Dashboard/index.tsx
+++ web/frontend/src/pages/Dashboard/index.tsx
@@ -1,10 +1,21 @@
-import { Container } from "react-bootstrap";
+import { Card, Container } from "react-bootstrap";
 import "./styles.scss";
 
 const Dashboard = () => {
   return (
-    <div className="content">
-      <Container className="dashboard-container"></Container>
+    <div className="content mh-100">
+      <Card className="h-100 mh-100 dashboard-card max d-flex flex-column">
+        <Card className="w-auto h-auto">
+          <Card.Header>
+            <Card.Title>Live Time Camera</Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <Container>
+              <h2>Welcome to your admin panel</h2>
+            </Container>
+          </Card.Body>
+        </Card>
+      </Card>
     </div>
   );
 };
